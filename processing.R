3d8d1740c9225b664d8d528bcf2f781f6811aa93
raphaelgall <raphael.gall@soma-analytics.com>
2016-09-01 19:20:23 +0100

    Code not functional yet

--- processing.R
+++ processing.R
@@ -2,95 +2,176 @@
 #explorative analysis, including machine learning, quantitative text analysis.
 #processing and computation
 
-#set boolean switch to execute full control flow for nobel_literature and then re-run for nobel_peace
-#switched <- FALSE
+#plotting frame for all nobel prizes
+years          <- seq(1940, 2010, 10)
+frequencies    <- c(1:length(years))
 
-#write a loop for 2nd nobel prize for peace.
-# for (switched == FALSE, 1:2) {
-#   nobel_literature <- nobel_literature
-#   
-# } else {
-#   nobel_literature <- nobel_peace }
+#############Part A: processing peace prize
+
+#all speeches
+nobel_prize    <- nobel_peace
 
 #preprocessing
 #remove number, capitalisation, common words, puntucation, other meaningless bits.
-
-nobel_literature  <- tm_map(nobel_literature, removePunctuation)
-nobel_literature  <- tm_map(nobel_literature, removeNumbers)
-nobel_literature  <- tm_map(nobel_literature, tolower)#lowercase
-nobel_literature  <- tm_map(nobel_literature, removeWords, stopwords("english"))
-nobel_literature  <- tm_map(nobel_literature, stemDocument)
-nobel_literature  <- tm_map(nobel_literature, stripWhitespace)
-nobel_literature  <- tm_map(nobel_literature, PlainTextDocument)
+nobel_prize  <- tm_map(nobel_prize, removePunctuation)
+nobel_prize  <- tm_map(nobel_prize, removeNumbers)
+nobel_prize  <- tm_map(nobel_prize, tolower)#lowercase
+nobel_prize  <- tm_map(nobel_prize, removeWords, stopwords("english"))
+nobel_prize  <- tm_map(nobel_prize, stemDocument)
+nobel_prize  <- tm_map(nobel_prize, stripWhitespace)
+nobel_prize  <- tm_map(nobel_prize, PlainTextDocument)
 
 #stage the data
-dtm  <- DocumentTermMatrix(nobel_literature)
-tdm  <- TermDocumentMatrix(nobel_literature)
-
-#explore data
-freq <- colSums(as.matrix(dtm))
-length(freq)
-ord <- order(freq)
+dtm   <- DocumentTermMatrix(nobel_prize)
+tdm   <- TermDocumentMatrix(nobel_prize)
 
 #removing sparse terms
-dtms <- removeSparseTerms(dtm, 0.1)
-
-#frequencies in multiple ways.
-freq[head(ord)]
-freq[tail(ord)]   
-head(table(freq), 20)   
-tail(table(freq), 20)   
-
-freq <- colSums(as.matrix(dtms))   
-freq
-freq <- sort(colSums(as.matrix(dtm)), decreasing=TRUE)   
-mostfrequentwords <- head(freq, 25)   
-barplot(mostfrequentwords, las = 2, names.arg = names(mostfrequentwords),
-col ="lightblue", main ="Most frequent words",
-ylab = "Word frequencies")
-
-findFreqTerms(dtm, lowfreq=50) 
-wf    <- data.frame(word=names(freq), freq=freq)   
-head(wf)  
-
-frequencyplot <- ggplot(subset(wf, freq>50), aes(word, freq))    
-frequencyplot <- frequencyplot + geom_bar(stat="identity")   
-frequencyplot <- frequencyplot + theme(axis.text.x=element_text(angle=45, hjust=1))   
-
-
-
-#Relationships Between Terms Term Correlations
-
-#word clouds
-set.seed(142)   
-dark2 <- brewer.pal(6, "Dark2")   
-wordcloudcolored <- wordcloud(names(freq), freq, max.words=100, rot.per=0.2, colors=dark2)   
+dtms  <- removeSparseTerms(dtm, 0.1)
 
 #Clustering by Term Similarity
-dtmss <- removeSparseTerms(dtm, 0.15) # This makes a matrix that is only 15% empty space, maximum.   
+dtmss <- removeSparseTerms(dtm, 0.15) # This makes a matrix that is only 15% empty space, maximum.
 
-#Hierarchal Clustering
-d     <- dist(t(dtmss), method="euclidian")   
-fit   <- hclust(d=d, method="ward")   
-#fit   
-#plot(fit, hang=-1)   
-#plot.new()
-# plot(fit, hang=-1)
-# groups <- cutree(fit, k=5)   # "k=" defines the number of clusters you are using   
-# rect.hclust(fit, k=5, border="red") # draw dendogram with red borders around the 5 clusters   
-# 
-# #k-means clustering
-# dclust  <- dist(t(dtmss), method="euclidian")   
-# kfit    <- kmeans(dclust, 5)   
-# clusplot(as.matrix(d), kfit$cluster, color=T, shade=T, labels=2, lines=0)   
+#findFreqTerms(dtms, lowfreq=50)
+freq  <- colSums(as.matrix(dtms))
+freq
+freq  <- sort(colSums(as.matrix(dtm)), decreasing = TRUE)
+wf    <- data.frame(word = names(freq), freq = freq)
 
-#For loop switch condition
-# switched <- TRUE
-# 
-#}
+#frequencies are key outcome of the pre-processing
+mostfrequentwords               <- wf[1:50,]
+peace_mostfrequentwords15       <- freq[tail(ord)]
 
-#Sentiment analysis[edit]
+##### Sub-analysis for frequencies by decade
+#top 5 peace word frequencies per year
+frequency_war             <- frequencies #red
+frequency_world           <- frequencies #blue
+frequency_peace           <- frequencies #green
+frequency_international   <- frequencies #black
+frequency_one             <- frequencies #white
+
+for (i in 1:length(nobel_peace_bydecade)) {
+  count       <- i
+  nobel_prize <- nobel_peace_bydecade[[count]]
+  
+  #preprocessing
+  #remove number, capitalisation, common words, puntucation, other meaningless bits.
+  nobel_prize  <- tm_map(nobel_prize, removePunctuation)
+  nobel_prize  <- tm_map(nobel_prize, removeNumbers)
+  nobel_prize  <- tm_map(nobel_prize, tolower)#lowercase
+  nobel_prize  <- tm_map(nobel_prize, removeWords, stopwords("english"))
+  nobel_prize  <- tm_map(nobel_prize, stemDocument)
+  nobel_prize  <- tm_map(nobel_prize, stripWhitespace)
+  nobel_prize  <- tm_map(nobel_prize, PlainTextDocument)
+  
+  #stage the data
+  dtm   <- DocumentTermMatrix(nobel_prize)
+  tdm   <- TermDocumentMatrix(nobel_prize)
+  
+  #removing sparse terms
+  dtms  <- removeSparseTerms(dtm, 0.1)
+
+  #findFreqTerms(dtms, lowfreq=50)
+  freq  <- colSums(as.matrix(dtms))
+  freq
+  freq  <- sort(colSums(as.matrix(dtm)), decreasing = TRUE)
+  wf    <- data.frame(word = names(freq), freq = freq)
+  mostfrequentwords <- wf[1:50,]
+  
+  #create vectors of top 5 most freq words
+  frequency_war[count]            <- mostfrequentwords['war','freq']
+  frequency_world[count]          <- mostfrequentwords['world','freq']
+  frequency_peace[count]          <- mostfrequentwords['peace','freq']
+  frequency_international[count]  <- mostfrequentwords['international','freq']
+  frequency_one[count]            <- mostfrequentwords['one','freq']
+}
+
+
+#data frames for plotting
+peace_word_frequencies_topfive  <- data.frame(years, frequency_war, frequency_world, frequency_peace, frequency_international, frequency_one)
+peace_frequencies_all           <- data.frame(frequency_war, frequency_world, frequency_peace, frequency_international, frequency_one)
+
+#scatterplotmatrix 
+peace_relationshipsbetweenwords <- pairs.panels(peace_frequencies_all)
+
+
+
+
+
+
+
+
+#############Part B: processing literature prize
+
+
+#top 5 literature word frequencies per year
+# frequency_war            <- frequencies #red
+# frequency_world          <- frequencies #blue
+# frequency_peace          <- frequencies #green
+# frequency_international  <- frequencies #black
+# frequency_one            <- frequencies #white
+
+for (i in 1:length(nobel_literature_bydecade)) {
+  count       <- i
+  nobel_prize <- nobel_literature_bydecade[[count]]
+  
+  #preprocessing
+  #remove number, capitalisation, common words, puntucation, other meaningless bits.
+  nobel_prize  <- tm_map(nobel_prize, removePunctuation)
+  nobel_prize  <- tm_map(nobel_prize, removeNumbers)
+  nobel_prize  <- tm_map(nobel_prize, tolower)#lowercase
+  nobel_prize  <- tm_map(nobel_prize, removeWords, stopwords("english"))
+  nobel_prize  <- tm_map(nobel_prize, stemDocument)
+  nobel_prize  <- tm_map(nobel_prize, stripWhitespace)
+  nobel_prize  <- tm_map(nobel_prize, PlainTextDocument)
+  
+  #stage the data
+  dtm  <- DocumentTermMatrix(nobel_prize)
+  tdm  <- TermDocumentMatrix(nobel_prize)
+  
+  #removing sparse terms
+  dtms <- removeSparseTerms(dtm, 0.1)
+  
+  #findFreqTerms(dtms, lowfreq=50)
+  freq <- colSums(as.matrix(dtms))
+  freq
+  freq <- sort(colSums(as.matrix(dtm)), decreasing = TRUE)
+  wf    <- data.frame(word = names(freq), freq = freq)
+  mostfrequentwords <- wf[1:50,]
+  
+  #create vectors of top 5 most freq words
+  frequency_war[count]            <- mostfrequentwords['war','freq']
+  frequency_world[count]          <- mostfrequentwords['world','freq']
+  frequency_peace[count]          <- mostfrequentwords['peace','freq']
+  frequency_international[count]  <- mostfrequentwords['international','freq']
+  frequency_one[count]            <- mostfrequentwords['one','freq']
+}
+
+#data frames for plotting
+literature_word_frequencies_topfive  <- data.frame(years, frequency_war, frequency_world, frequency_peace, frequency_international, frequency_one)
+literature_frequencies_all           <- data.frame(frequency_war, frequency_world, frequency_peace, frequency_international, frequency_one)
+
+#scatterplotmatrix 
+literature_relationshipsbetweenwords <- pairs.panels(literature_frequencies_all)
+
+
+#########Part C: Sentiment analysis in future
 # Sentiment analysis[edit]
 # Sentiment analysis may involve analysis of movie reviews for estimating how favorable a review is for a movie.[20] Such an analysis may need a labeled data set or labeling of the affectivity of words. Resources for affectivity of words and concepts have been made for WordNet[21] and ConceptNet,[22] respectively.
-# 
-# Text has been used to detect emotions in the related area of affective computing.[23] Text based approaches to affective computing have been used on multiple corpora such as students evaluations, children stories and news stories.
+#
+# # Text has been used to detect emotions in the related area of affective computing.[23] Text based approaches to affective computing have been used on multiple corpora such as students evaluations, children stories and news stories.
+# install.packages('syuzhet')
+# library('syuzhet')
+#
+# get_dct_transform(dtm, low_pass_size = 5, x_reverse_len = 100, scale_vals = FALSE, scale_range = FALSE)
+#
+# str(nobel_literature)
+# head(nobel_literature)
+# get_nrc_sentiment(nobel_literature)
+
+### end of processing 
+
+#### Plotting all 
+
+#plot
+
+
--- source.R
+++ source.R
@@ -3,43 +3,81 @@
 #source file
 
 #libraries, installed only once.
-#neededpackages   <- c("tm", "SnowballCC", "RColorBrewer", "ggplot2", "wordcloud", "biclust", "cluster", "igraph", "fpc")   
-#install.packages(neededpackages, dependencies=TRUE)   
+#neededpackages   <- c("tm", "SnowballCC", "RColorBrewer", "ggplot2", "wordcloud", "biclust", "cluster", "igraph", "fpc", "psych", "ggvis")
+#install.packages(neededpackages, dependencies=TRUE)
 
 library('tm')
-library('wordcloud')   
+library('wordcloud')
 library('SnowballC')
-library('ggplot2')   
-library('cluster')   
-library('fpc')   
+library('ggplot2')
+library('cluster')
+library('fpc')
+library('psych')
+library('ggvis')
 
-#install.packages("Rcampdf", repos = "http://datacube.wu.ac.at/", type = "source")   
-
-
-# # devtools required to install quanteda from Github
-# if (!require(devtools)) install.packages("devtools")
-# library(devtools)   
-# # install the latest version quanteda from Github
-# install_github("quanteda", username="kbenoit", dependencies=TRUE, quick=TRUE)
-# 
-# 
-# 
-# library(quanteda)
-# # create a corpus from the immigration texts from UK party platforms
-# uk2010immigCorpus <- corpus(uk2010immig,
-#                             docvars=data.frame(party=names(uk2010immig)),
-#                             notes="Immigration-related sections of 2010 UK party manifestos",
-#                             enc="UTF-8")
-# 
-#setwd
-#getwd()
-
-#load texts
-peacedata           <-   file.path("nobelprize_pea/1940_2016")   
-literaturedata      <-   file.path("nobelprize_lit/1949_2016")   
+#load texts for all years per prize
+peacedata           <- file.path("nobelprize_pea/1940_2016")
+literaturedata      <- file.path("nobelprize_lit/1949_2016")
 
 nobel_peace         <- Corpus(DirSource(peacedata))
 nobel_literature    <- Corpus(DirSource(literaturedata))
 
-#summary(nobel_literature)
+#load texts for each decade separatly per prize
+peacedata_1940      <-   file.path("nobelprize_pea/1940")
+peacedata_1950      <-   file.path("nobelprize_pea/1950")
+peacedata_1960      <-   file.path("nobelprize_pea/1960")
+peacedata_1970      <-   file.path("nobelprize_pea/1970")
+peacedata_1980      <-   file.path("nobelprize_pea/1980")
+peacedata_1990      <-   file.path("nobelprize_pea/1990")
+peacedata_2000      <-   file.path("nobelprize_pea/2000")
+peacedata_2010      <-   file.path("nobelprize_pea/2010")
 
+nobel_peace_1940         <- Corpus(DirSource(peacedata_1940))
+nobel_peace_1950         <- Corpus(DirSource(peacedata_1950))
+nobel_peace_1960         <- Corpus(DirSource(peacedata_1960))
+nobel_peace_1970         <- Corpus(DirSource(peacedata_1970))
+nobel_peace_1980         <- Corpus(DirSource(peacedata_1980))
+nobel_peace_1990         <- Corpus(DirSource(peacedata_1990))
+nobel_peace_2000         <- Corpus(DirSource(peacedata_2000))
+nobel_peace_2010         <- Corpus(DirSource(peacedata_2010))
+
+literaturedata_1940      <-   file.path("nobelprize_lit/1940")
+literaturedata_1950      <-   file.path("nobelprize_lit/1950")
+literaturedata_1960      <-   file.path("nobelprize_lit/1960")
+literaturedata_1970      <-   file.path("nobelprize_lit/1970")
+literaturedata_1980      <-   file.path("nobelprize_lit/1980")
+literaturedata_1990      <-   file.path("nobelprize_lit/1990")
+literaturedata_2000      <-   file.path("nobelprize_lit/2000")
+literaturedata_2010      <-   file.path("nobelprize_lit/2010")
+
+nobel_literature_1940         <- Corpus(DirSource(literaturedata_1940))
+nobel_literature_1950         <- Corpus(DirSource(literaturedata_1950))
+nobel_literature_1960         <- Corpus(DirSource(literaturedata_1960))
+nobel_literature_1970         <- Corpus(DirSource(literaturedata_1970))
+nobel_literature_1980         <- Corpus(DirSource(literaturedata_1980))
+nobel_literature_1990         <- Corpus(DirSource(literaturedata_1990))
+nobel_literature_2000         <- Corpus(DirSource(literaturedata_2000))
+nobel_literature_2010         <- Corpus(DirSource(literaturedata_2010))
+
+#store speeches by decades in list
+nobel_peace_bydecade <- list(
+  nobel_peace_1940,
+  nobel_peace_1950,
+  nobel_peace_1960,
+  nobel_peace_1970,
+  nobel_peace_1980,
+  nobel_peace_1990,
+  nobel_peace_2000,
+  nobel_peace_2010
+)
+
+nobel_literature_bydecade <- list(
+  nobel_literature_1940,
+  nobel_literature_1950,
+  nobel_literature_1960,
+  nobel_literature_1970,
+  nobel_literature_1980,
+  nobel_literature_1990,
+  nobel_literature_2000,
+  nobel_literature_2010
+)
